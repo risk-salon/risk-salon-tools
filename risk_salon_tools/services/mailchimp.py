224e45c82a1321b044810cb4d501b4c89e87b008
Nate Kugland <nate.kugland@airbnb.com>
2017-12-25 18:17:29 -0800

    added order methods

--- risk_salon_tools/services/mailchimp.py
+++ risk_salon_tools/services/mailchimp.py
@@ -1,8 +1,12 @@
+import datetime
+
 import pandas as pd
 
 from collections import OrderedDict
 from mailchimp3 import MailChimp
+
 from risk_salon_tools.services.config import Settings
+from risk_salon_tools.utilities import ordinal_date_suffix
 
 
 class MailChimpClient(object):
@@ -16,17 +20,20 @@ class MailChimpList(MailChimpClient):
     def __init__(self, list_id):
         super().__init__()
         self.list_id = list_id
+        self.eventbrite_store_id = 'eventbrite' + self.list_id
+
         self._merge_fields = self.client.lists.merge_fields.get(list_id=self.list_id,
                                                                 merge_id='')
         self._field_name_topics_survey = 'TOPICSSURV'
+        self._members_all = None
 
     def _get_merge_fields_map(self):
         return {x['tag']: x['name'] for x in self._merge_fields['merge_fields']}
 
     def get_active_members_df(self, map_field_names=True):
-        _members_all = self.client.lists.members.all(list_id=self.list_id)
+        self._members_all = self.client.lists.members.all(list_id=self.list_id)
         members = []
-        for mm in _members_all['members']:
+        for mm in self._members_all['members']:
             m = OrderedDict()
             m.update(OrderedDict([(k, mm[k]) for k in ('id', 'email_address', 'status')]))
 
@@ -46,8 +53,11 @@ class MailChimpList(MailChimpClient):
                                              subscriber_hash=member_id,
                                              fields="merge_fields")['merge_fields']
 
-    def set_topics_survey_status(self, member_id, status):
-        assert(status in ['yes', 'no'])
+    def set_member_topics_survey_status(self, member_id, status):
+        try:
+            status in ['yes', 'no']
+        except ValueError:
+            raise ValueError("Incorrect status value, should be either 'yes' or 'no'")
 
         merge_fields = self.get_member_merge_fields(member_id)
 
@@ -56,3 +66,32 @@ class MailChimpList(MailChimpClient):
         payload = {'merge_fields': merge_fields}
         self.client.lists.members.update(list_id=self.list_id,
                                          subscriber_hash=member_id, data=payload)
+
+    def _format_eventbrite_date(self, date):
+        try:
+            dt = datetime.datetime.strptime(date, '%Y-%m-%d')
+        except ValueError:
+            raise ValueError("Incorrect date format, should be YYYY-MM-DD")
+
+        return '{dt_first} {dt_day_ordinal}'.format(
+            dt_first=dt.strftime('%a, %b'),
+            dt_day_ordinal=ordinal_date_suffix(dt.day)).upper()
+
+    def _test_format_eventbrite_date(self):
+        assert(self._format_eventbrite_date('2017-12-17') == 'SUN, DEC 17TH')
+
+    def get_members_with_eventbrite_orders(self, event_date, event_name):
+        event_date_formatted = self._format_eventbrite_date(event_date)
+
+        def has_product(lines, event_date, event_name):
+            for line in lines:
+                line_date, line_name, line_ticket_name = line['product_title'].split(' - ')
+                if line_date == event_date and line_name == event_name:
+                    return True
+
+        orders = self.client.stores.orders.all(store_id='eventbrite'+self.list_id,
+                                               get_all=True)['orders']
+
+        return [x['customer']['email_address']
+                for x in orders
+                if has_product(x['lines'], event_date_formatted, event_name)]
